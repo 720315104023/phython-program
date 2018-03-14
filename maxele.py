d6a30115c81dc57978f6ead12be8cbc6b31c07a6
720315104023 <ponmaniarumugam98@gmail.com>
2018-03-14 16:18:24 +0530

    Create maxele.py

---
+++ maxele.py
@@ -0,0 +1,8 @@
+list = []
+num = int(input('How many numbers: '))
+
+for n in range(num):
+    numbers = int(input('Enter number '))
+    list.append(numbers)
+
+print("Maximum element in the list is :", max(lst))
