64f062f9388c33018f9a1689024bb63b7da5e4cd
720315104023 <ponmaniarumugam98@gmail.com>
2018-03-20 16:41:08 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,7 @@
+x = int(input("enter the number1"))
+y = int(input("enter the number2"))
+temp = x
+x = y
+y = temp
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
